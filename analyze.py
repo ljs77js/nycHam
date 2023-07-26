c1b51ec4b94f8443e9d5067fbbe896a07e268c09
ljs77js <ljs77js@naver.com>
2023-07-26 17:07:19 +0900

    test nyc data  api added

--- analyze.py
+++ analyze.py
@@ -1,20 +1,17 @@
 import pandas as pd
 
-# Load the Excel file
-df = pd.read_excel('your_file.xlsx', engine='openpyxl')
+# Load the CSV file
+df = pd.read_csv('manhattan_data.csv', low_memory=False)
 
-# Sort the data based on category
-df = df.sort_values(by='your_category')
-
-# Filter out the columns you're interested in
-df_filtered = df[['ZIPCODE', 'INCIDENT_DATETIME', 'INITIAL_CALL_TYPE', 'FIRST_TO_HOSP_DATETIME']]
+# Filter out the columns you're interested in and create a copy
+df_filtered = df[['ZIPCODE', 'FIRST_HOSP_ARRIVAL_DATETIME', 'INITIAL_CALL_TYPE', 'FIRST_TO_HOSP_DATETIME']].copy()
 
 # Convert dates from string to datetime
-df_filtered['INCIDENT_DATETIME'] = pd.to_datetime(df_filtered['INCIDENT_DATETIME'])
-df_filtered['FIRST_TO_HOSP_DATETIME'] = pd.to_datetime(df_filtered['FIRST_TO_HOSP_DATETIME'])
+df_filtered['FIRST_HOSP_ARRIVAL_DATETIME'] = pd.to_datetime(df_filtered['FIRST_HOSP_ARRIVAL_DATETIME'], format='%Y %b %d %I:%M:%S %p')
+df_filtered['FIRST_TO_HOSP_DATETIME'] = pd.to_datetime(df_filtered['FIRST_TO_HOSP_DATETIME'], format='%Y %b %d %I:%M:%S %p')
 
 # Compute the time it took to get to the hospital
-df_filtered['TIME_TO_HOSPITAL'] = df_filtered['FIRST_TO_HOSP_DATETIME'] - df_filtered['INCIDENT_DATETIME']
+df_filtered['TIME_TO_HOSPITAL'] = df_filtered['FIRST_TO_HOSP_DATETIME'] - df_filtered['FIRST_HOSP_ARRIVAL_DATETIME']
 
-# Write the filtered and sorted DataFrame to a new Excel file
-df_filtered.to_excel('sorted_data.xlsx', index=False, engine='openpyxl')
+# Write the filtered DataFrame to a new CSV file
+df_filtered.to_csv('manhattan_sorted_data.csv', index=False)
--- mansort.py
+++ mansort.py
@@ -2,16 +2,25 @@ import pandas as pd
 
 class DataFilter:
     def __init__(self, file_path):
-        self.file_path = file_path
-        self.df = pd.read_csv(file_path)
+        self.df = pd.read_csv(file_path, low_memory=False, parse_dates=['FIRST_TO_HOSP_DATETIME', 'FIRST_HOSP_ARRIVAL_DATETIME'])
 
     def filter_by_borough(self, borough_name):
         self.df = self.df.loc[self.df['BOROUGH'] == borough_name]
 
+    def select_columns(self, columns):
+        self.df = self.df[columns]
+
+    def convert_datetime_format(self):
+        for column in ['FIRST_TO_HOSP_DATETIME', 'FIRST_HOSP_ARRIVAL_DATETIME']:
+            self.df[column] = self.df[column].dt.strftime('%Y-%m-%d %H:%M:%S')
+
     def save(self, output_file):
-        self.df.to_csv(output_file, index=False)
+        # Save only the first 100 rows
+        self.df.head(100).to_csv(output_file, index=False)
 
 # Using the class
-data_filter = DataFilter('./EMS/EMS_Incident_Dispatch_Data.csv')
+data_filter = DataFilter('./EMS_Incident_Dispatch_Data.csv')
 data_filter.filter_by_borough('MANHATTAN')
+data_filter.select_columns(['ZIPCODE', 'FIRST_TO_HOSP_DATETIME', 'FIRST_HOSP_ARRIVAL_DATETIME', 'INITIAL_CALL_TYPE'])
+data_filter.convert_datetime_format()
 data_filter.save('manhattan_data.csv')
